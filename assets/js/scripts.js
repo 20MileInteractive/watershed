112bb88b68fc569a46e53bac3e10e6c7d6946409
Sue <smalomo@mobiquityinc.com>
2013-09-16 13:00:01 -0400

    smooth scrolling to watershed & map images

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+	$('#find-watershed').click(function(e){
+		$('#my-watershed-img').slideDown('slow');
+		e.preventDefault();
+		var watershed_top = $("#my-watershed-img").offset().top;
+		// console.log(watershed_top);
+		$('body').animate({scrollTop:watershed_top}, 2000, 'swing');
+	});
+	$('#find_on_map').click(function(e){
+		$('#map-img').slideDown('slow');
+		e.preventDefault();
+		var map_top = $('#map-img').offset().top;
+		$('body').animate({scrollTop:map_top}, 2000, 'swing');
+	});
+});
