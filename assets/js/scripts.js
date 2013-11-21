f571f26ce11b0dd1543ae5887b3b456ade60b053
David Fontes <dfontes@20miletech.com>
2013-11-21 10:43:17 -0500

    Show/Hide Results fix
    added jquery to animate margin-bottom of map, so no white space when
    results are hidden

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,6 +10,17 @@ $(document).ready(function(){
 		// console.log(watershed_top);
 		// $('body').animate({scrollTop:watershed_top}, 2000, 'swing');
 	});
+
+
+	$('#show_results').click(function(e){
+		var currentMargin = $('.community-map').css('margin-bottom');
+		if (currentMargin == '245px') {
+			$('.community-map').animate({ 'marginBottom': '53px' }, 390);
+		} else {
+			$('.community-map').animate({ 'marginBottom': '245px' }, 390);
+		}	
+		e.preventDefault();
+	});
 	
 
 	$.stellar({responsive: false});
